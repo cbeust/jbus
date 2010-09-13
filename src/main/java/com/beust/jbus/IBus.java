616eed0a068d39da185c85e486e632569dae6f12
Cedric Beust <cbeust@linkedin.com>
2010-09-13 14:00:13 -0700

    Updated pom, added comments to IBus.

--- src/main/java/com/beust/jbus/IBus.java
+++ src/main/java/com/beust/jbus/IBus.java
@@ -3,12 +3,29 @@ package com.beust.jbus;
 public interface IBus
 {
 
-  public abstract void register(Object object);
+  /**
+   * Register this object to receive notifications. The object passed in parameter will
+   * be searched for any method annotated with @{link @Subscriber} and each of
+   * these methods will be invoked if an object posted on the bus matches its
+   * parameter.
+   */
+  void register(Object object);
 
-  public abstract void unregister(Object object);
+  /**
+   * Unregister an object from the bus.
+   */
+  void unregister(Object object);
 
-  public abstract void post(Object event);
+  /**
+   * Post an object on the bus.
+   */
+  void post(Object event);
 
-  public abstract void post(Object event, String[] categories);
+  /**
+   * Post an object on the bus with the specified categories. Only subscribers
+   * whose category pattern matches at least one of these categories will
+   * be invoked.
+   */
+  void post(Object event, String[] categories);
 
 }
